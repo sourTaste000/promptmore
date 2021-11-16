12c17ce990a2bf09c9caac29f1ccb2266bdb7cbf
sussyimpostor <47074495+sourTaste000@users.noreply.github.com>
2021-11-16 10:46:43 -0800

    poger

--- promptmore.sh
+++ promptmore.sh
@@ -1,5 +1,5 @@
 # shellcheck disable=SC2148
-# TODO: Add support for other shells, support multiple 'extras', eg. displaying the git branch ad the node project version at the same time
+# TODO: Add support for other shells
 function batt_level() {
     level=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
     if [ "$level" -lt 10 ]; then
@@ -25,14 +25,15 @@ function batt_level() {
     fi
 }
 
+extras=""
+
 function node_project_ver() {
-    extras=""
     if [ -f "package.json" ]; then
         # test if package.json has a version property
         if [ -n "$(jq -r '.version' package.json)" ]; then
-            extras="on %F{yellow} $(jq -r '.version' package.json)%F{white} using %F{green} $(node -v)%F{white} "
+            extras+="on %F{yellow} $(jq -r '.version' package.json)%F{white} using %F{green} $(node -v)%F{white} "
         else
-            extras="on %F{green} Unknown%F{white} using %F{green} $(node -v)%F{white} "
+            extras+="on %F{green} Unknown%F{white} using %F{green} $(node -v)%F{white} "
         fi
     fi
     PS1="%K{magenta}%F{black}%~%f%k $extras%(?.%(!.# .➜ ).%(!.%F{red}#%? %F{white}.%F{red}➜%? %F{white}))"
@@ -40,9 +41,8 @@ function node_project_ver() {
 }
 
 function git_project_ver() {
-    extras=""
     if [ -d ".git" ]; then
-        extras="on %F{cyan}$(git branch | sed 's/*//')%F{white} "
+        extras+="via %F{cyan}$(git branch | sed 's/*//')%F{white} "
         if [ -n "$(git status --porcelain)" ]; then
             extras+="with %F{red}$(git status --porcelain | wc -l | xargs)%F{white} uncommitted changes "
         fi
@@ -51,6 +51,15 @@ function git_project_ver() {
     export PS1
 }
 
+function reset() {
+    if [ "$OLDPWD" != "$(pwd)" ]; then
+        extras=""
+        PS1="%K{magenta}%F{black}%~%f%k $extras%(?.%(!.# .➜).%(!.%F{red}#%? %F{white}.%F{red}➜%? %F{white})) "
+        export PS1
+    fi
+}
+
+
 PS1="%K{magenta}%F{black}%~%f%k %(?.%(!.# .➜ ).%(!.%F{red}#%? %F{white}.%F{red}➜%? %F{white}))"
 PS3="select>"
 RPROMPT="%* | $(batt_level)"
@@ -58,4 +67,4 @@ RPROMPT="%* | $(batt_level)"
 export PS1
 export PS3
 export RPROMPT
-precmd_functions+=( node_project_ver git_project_ver )
+chpwd_functions+=( reset node_project_ver git_project_ver )
